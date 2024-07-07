077a75dc1c378ce8babe4fa1b14937191cccd6ce
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-06 17:39:37 -0700

    commit

--- dbcon.php
+++ dbcon.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=judging', 'root', '');
+$conn = new PDO('mysql:host=127.0.0.1;dbname=judging', 'judgin_root', '1Judging_root');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
