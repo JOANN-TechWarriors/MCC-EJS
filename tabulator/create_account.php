4283fb724f2178e06a1b3f9e9cbc7efef6412210
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-15 07:49:05 -0700

    commit

--- tabulator/create_account.php
+++ tabulator/create_account.php
@@ -127,7 +127,7 @@
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="..//assets/js/ie10-viewport-bug-workaround.js"></script>
-    <script src="javascript/jquery1102.min.js"></script>
+    <script src="..//javascript/jquery1102.min.js"></script>
     
     
     
