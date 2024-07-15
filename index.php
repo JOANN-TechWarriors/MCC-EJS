b589d2b782e5ff6a1b73a81350cad470f1fe9406
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-15 08:19:10 -0700

    commit

--- index.php
+++ index.php
@@ -250,7 +250,7 @@
     <div class="container">
         <div class="logo">
             <a href="#" style="font-family: impact; color: #1153D0;">
-                <img src="assets/img/mcc_logo.png" style="height: 40px;  vertical-align: middle;"> Event Judging
+                <img src="assets/img/mcc_logo.png" style="height: 40px;  vertical-align: middle;"> MCC Event
             </a>
              <span class="text-light"MCC>
         </div>
