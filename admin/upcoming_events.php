e1055864bcaeb9b84139444407615dfba52d89a2
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-19 02:02:14 -0700

    commit

--- admin/upcoming_events.php
+++ admin/upcoming_events.php
@@ -139,7 +139,15 @@
     <li><a href="#" id="logout"><i class="fas fa-sign-out-alt"></i> <span>LOGOUT</span></a></li>
   </ul>
 </div
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+<script>
+  document.getElementById('toggle-btn').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('minimized');
+    document.getElementById('content').classList.toggle('minimized');
+    document.getElementById('toggle-btn').classList.toggle('minimized');
+  });
+</script>
   <div class="content">
     <div class="container">
       <div class="row">
@@ -373,17 +381,7 @@
     function datetimeLocal(dateTimeStr) {
       return moment(dateTimeStr).format('YYYY-MM-DDTHH:mm');
     }
-  </script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
-<script>
-  document.getElementById('toggle-btn').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('minimized');
-    document.getElementById('content').classList.toggle('minimized');
-    document.getElementById('toggle-btn').classList.toggle('minimized');
-  });
-</script>
+  
 
 </body>
 </html>
