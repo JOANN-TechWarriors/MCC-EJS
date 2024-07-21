5a2493ed72020984d7c1797f0e0b7d18fcfc9d95
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-21 02:59:56 -0700

    commit

--- admin/index.php
+++ admin/index.php
@@ -113,8 +113,8 @@ thead th {
   </div>
 </div>
    
-   <div class="modal fade" id="forgot-password-modal" tabindex="-1" role="dialog" aria-labelledby="forgot-password-modal-label" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+   <div class="modal fade" id="forgot-password-modal" tabindex="-1" role="dialog" aria-labelledby="forgot-password-modal-label" >
+  <div class="modal-dialog" role="dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="forgot-password-modal-label">Forgot Password?</h5>
