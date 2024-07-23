4c9bfe9965f8e6284cc552c2f30fb71ce13c1f7f
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-23 02:56:05 -0700

    commit

--- admin/upcoming_events.php
+++ admin/upcoming_events.php
@@ -188,8 +188,8 @@
             </form>
           </div>
           <div class="modal-footer">
+          <button type="button" class="btn btn-success" id="addEventButton">Save</button>
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="cancelEventButton">Cancel</button>
-            <button type="button" class="btn btn-success" id="addEventButton">Save</button>
           </div>
         </div>
       </div>
@@ -219,9 +219,9 @@
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="cancelEventButton">Cancel</button>
-            <button type="button" class="btn btn-danger" id="deleteEventButton">Delete</button>
             <button type="button" class="btn btn-success" id="updateEventButton">Update</button>
+            <button type="button" class="btn btn-danger" id="deleteEventButton">Delete</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="cancelEventButton">Cancel</button>
           </div>
         </div>
       </div>
