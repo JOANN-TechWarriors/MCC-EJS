2f08ce8f30144b7380aa98af0f4650ad4afa8c6d
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-07-24 13:36:03 -0700

    commit

--- admin/home.php
+++ admin/home.php
@@ -24,7 +24,7 @@
         left: 0;
         height: 100%;
         width: 250px;
-        background-color: #27293d;
+        background-color: transparent;
         color: #fff;
         padding-top: 20px;
         transition: all 0.3s;
