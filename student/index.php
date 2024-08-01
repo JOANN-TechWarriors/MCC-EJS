f886eaaa2ddada7255e4baf5e160d9cd9574eb1b
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-08-01 14:23:03 -0700

    commit

--- student/index.php
+++ student/index.php
@@ -22,10 +22,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $redirect_url = $_SESSION['redirect_after_login'];
                 unset($_SESSION['redirect_after_login']); // Clear the stored URL
                 $_SESSION['login_success'] = true;
-                header("Location: ../index.php" . $redirect_url);
+                header("Location: ../" . $redirect_url);
             } else {
                 $_SESSION['login_success'] = true;
-                header("Location: ../poll/index.php"); // Default redirect if no stored URL
+                header("Location: ../index.php"); // Default redirect if no stored URL
             }
             exit();
         } else {
