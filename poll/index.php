ea2e245bbcb5f212f34bf965425e3257eba69261
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-08-01 13:06:26 -0700

    commit

--- poll/index.php
+++ poll/index.php
@@ -71,7 +71,7 @@ $has_voted = $stmt->fetch(PDO::FETCH_ASSOC);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="../ejs_logo.png"/>
+    <link rel="shortcut icon" href="../img/logo.png"/>
     <title>POLL VOTE</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
