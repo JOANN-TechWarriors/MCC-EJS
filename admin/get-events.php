8afe675f4148d610cbec3656fb4cf7c8c0c2fab5
JOANN-TechWarriors <joannrebamonte80@gmail.com>
2024-08-04 09:02:47 -0700

    commit

--- admin/get-events.php
+++ admin/get-events.php
@@ -2,6 +2,7 @@
 // Start the session (if not already started)
 session_start();
 
+// Connect to database
 $host = '127.0.0.1';
 $username = 'u510162695_judging_root';
 $password = '1Judging_root';  // Replace with the actual password
@@ -9,7 +10,6 @@ $dbname = 'u510162695_judging';
 
 
 $conn = new mysqli($host, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
